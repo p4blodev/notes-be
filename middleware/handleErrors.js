d2d67e17349c3860c54f68126446f1f6518c3d2b
Pablo Rodriguez <pablorodriguez833@gmail.com>
2022-09-17 13:32:08 -0300

    chore: improve erro handle middleware

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,4 +1,11 @@
 module.exports = (error, _req, res, _next) => {
-  console.log('error', error)
-  res.status(500).json({ error: error.name }).end()
+  console.log('🛑 handleError -> error', error)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  } else {
+    return res.status(400).json({ error: 'bad request' })
+  }
 }
